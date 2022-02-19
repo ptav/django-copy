c02d29354a4ddc2cb757a32ff0b2a9e0ac9228bf
ptav <7433752+ptav@users.noreply.github.com>
2022-02-19 17:22:11 +0000

    Remove dependency on django.contrib.auth migration

--- djangocopy/migrations/0007_auto_20220219_1612.py
+++ djangocopy/migrations/0007_auto_20220219_1612.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
         ('djangocopy', '0006_auto_20190810_1524'),
     ]
 
