e6424e97930ea523bcf52a7008246cb014caeef1
ptav <7433752+ptav@users.noreply.github.com>
2023-09-16 07:17:07 +0100

    Remove Django version restriction

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     include_package_data=True,
 
     install_requires=[
-        'django>=4.0',
+        'django',
         'django-simple-history',
         'django-ckeditor',
         'django-ckeditor-filer',
